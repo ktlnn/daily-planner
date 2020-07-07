bd69e7b332f199a776a9c65dbeaae8529b7b83db
Katelynn <ktlnn.nguyen@gmail.com>
2020-07-07 02:58:53 -0400

    displayed date

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,32 +1,32 @@
-$(document).ready(function(){
-    var moment = require('moment');
-    var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+$(document).ready(function () {
+    console.log(moment());
+    var currentDate = moment().format("L");
     var currentHour = moment().format("h");
     $("#currentDay").text(currentDate);
-    
-    
 
-   $(".save-button").click(function(){
-       console.log(this);
-       console.log(value);
-       var key = $(this).parent().attr("id");
+
+
+    $(".save-button").click(function () {
+        console.log(this);
+        console.log(value);
+        var key = $(this).parent().attr("id");
         var value = $(this).siblings("textarea").val();
         console.log(value);
         localStorage.setItem(key, value);
-       
-   })
 
-   var times = ["time-9", "time-10", "time-11"];
-   var displayPlanner = function(){
-    for(var i=0; i<times.length; i++){
-        console.log(times[i]);
-        
-       var value = (localStorage.getItem(times[i]));
-        $(`#${times[i]} textarea`).val(value);
+    })
+
+    var times = ["time-9", "time-10", "time-11", "time-12", "time-13", "time-14", "time-15", "time-16", "time-17"];
+    var displayPlanner = function () {
+        for (var i = 0; i < times.length; i++) {
+            console.log(times[i]);
+
+            var value = (localStorage.getItem(times[i]));
+            $(`#${times[i]} textarea`).val(value);
+        }
     }
-   }
-   displayPlanner();
-   
-  
-   
+    displayPlanner();
+
+
+
 });
