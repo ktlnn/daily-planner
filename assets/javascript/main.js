471e7bf640a82247d159a48c1768b6fc9db3e7c8
Katelynn <ktlnn.nguyen@gmail.com>
2020-07-06 22:55:34 -0400

    fixed time columns and added save-button function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,31 @@
-var toDoItems = [];
-var currentDate = moment().format("MMM Do");
-var currentHour = moment().format("h");
-var time = $(".time");
+$(document).ready(function(){
+    
+    var currentDate = moment().format("MMM Do");
+    var currentHour = moment().format("h");
+    
+    //localStorage.getItem(key);
 
-function background(){
-    $(".form-control").each(function (){
+   $(".save-button").click(function(){
+       console.log(this);
+       console.log(value);
+       var key = $(this).parent().attr("id");
+        var value = $(this).siblings("textarea").val();
+        console.log(value);
+        localStorage.setItem(key, value);
+       
+   })
+
+   var times = ["time-9", "time-10", "time-11"];
+   var displayPlanner = function(){
+    for(var i=0; i<times.length; i++){
+        console.log(times[i]);
         
-    })
-}
+       var value = (localStorage.getItem(times[i]));
+        $(`#${times[i]} textarea`).val(value);
+    }
+   }
+   displayPlanner();
+   
+  
+   
+});
